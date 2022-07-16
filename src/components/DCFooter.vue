d7d93cd0df7204809986a2cc1fb6394c83bab332
SamueleLettieri <samuele2073@gmail.com>
2022-07-16 16:11:58 +0200

    main e footer del progetto DC finiti

--- src/components/DCFooter.vue
+++ src/components/DCFooter.vue
@@ -1,6 +1,25 @@
 <template>
-  
+  <footer class="p-5">
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <div class="p-2 ms_border-button"><a class="ms_link" href="#">SIGN-UP NOW!</a></div>
+            <div>
+                <h3>
+                    <span class="ms_color me-3">FOLLOW US </span>
+                    <a class="ms-3" href="#"><img src="../assets/img/footer-facebook.png" alt=""></a>
+                    <a class="ms-3" href="#"><img src="../assets/img/footer-twitter.png" alt=""></a>
+                    <a class="ms-3" href="#"><img src="../assets/img/footer-youtube.png" alt=""></a>
+                    <a class="ms-3" href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a>
+                    <a class="ms-3" href="#"><img src="../assets/img/footer-periscope.png" alt=""></a>
+                </h3>
+            </div>
+        </div>
+    </div>
+  </footer>
 </template>
+
+
+
 
 <script>
 export default {
@@ -8,6 +27,27 @@ export default {
 }
 </script>
 
-<style>
+
+
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+
+footer{
+    background-color: $colorfooter;
+}
+
+.ms_border-button{
+    border: 2px solid $colorplus;
+}
+.ms_link{
+    color: $colorHeader;
+    text-decoration: none;
+}
+
+.ms_color{
+    color:$colorplus;
+}
 
 </style>
--- src/components/DCMain.vue
+++ src/components/DCMain.vue
@@ -1,5 +1,52 @@
 <template>
-  
+  <main class="">
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <div class="w-50 p-5">
+                <div class="ms_d-flex-collum">
+                    <h3 class="me-5">DC COMICS</h3>
+                    <ul>
+                        <li class="me-4"><a href="#">Characters</a></li>
+                        <li class="me-4"><a href="#">Comics</a></li>
+                        <li class="me-4"><a href="#">Movies</a></li>
+                        <li class="me-4"><a href="#">TV</a></li>
+                        <li class="me-4"><a href="#">Games</a></li>
+                        <li class="me-4"><a href="#">Videos</a></li>
+                        <li class="me-4"><a href="#">News</a></li>
+                    </ul>
+                    <h3 class="me-5">SHOP</h3>
+                    <ul>
+                        <li class="me-4"><a href="#">Shop DC</a></li>
+                        <li class="me-4"><a href="#">Shop DC Collection</a></li>
+                    </ul>
+                    <h3 class="me-5">DC</h3>
+                    <ul>
+                        <li class="me-4"><a href="#">Terms Of Use</a></li>
+                        <li class="me-4"><a href="#">Privacy Policy (New)</a></li>
+                        <li class="me-4"><a href="#">Ad Choices</a></li>
+                        <li class="me-4"><a href="#">Advertising</a></li>
+                        <li class="me-4"><a href="#">Jobs</a></li>
+                        <li class="me-4"><a href="#">Subscriptions</a></li>
+                        <li class="me-4"><a href="#">Talent Workshops</a></li>
+                        <li class="me-4"><a href="#">CPSC Certificates</a></li>
+                        <li class="me-4"><a href="#">Raitings</a></li>
+                        <li class="me-4"><a href="#">Shop Help</a></li>
+                        <li class="me-4"><a href="#">Contact Us</a></li>
+                    </ul>
+                    <h3 class="me-5">SITES</h3>
+                    <ul>
+                        <li class="me-4"><a href="#">DC</a></li>
+                        <li class="me-4"><a href="#">MAD Magazine</a></li>
+                        <li class="me-4"><a href="#">DC Kids</a></li>
+                        <li class="me-4"><a href="#">DC Universe</a></li>
+                        <li class="me-4"><a href="#">DC Power Visa</a></li>
+                    </ul>    
+                </div>
+            </div>
+            <div class="ms_img-Dc-box"><img class="ms_img-Dc" src="../assets/img/dc-logo-bg.png" alt=""></div>
+        </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -8,6 +55,46 @@ export default {
 }
 </script>
 
-<style>
 
+
+<style lang="scss" scoped>
+main{
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 400px;
+}
+
+h3{
+    color: white;
+    font-size: 20px;
+    padding-bottom: 2px;
+}
+
+.ms_d-flex-collum{ 
+    width: 55%; 
+    height: 345px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+}
+
+ul{
+    padding-left: 0;
+}
+
+
+a{
+  text-decoration: none;
+  color: rgb(158, 156, 156);
+  font-size: 12px;
+}
+
+.ms_img-Dc-box{
+    height: 400px;
+    width: 37%;
+}
+.ms_img-Dc{
+    width: 510px;
+    object-fit: cover;
+    height: 100%;
+}
 </style>
