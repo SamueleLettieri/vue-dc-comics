a9a309beb0b98ed7d274dd0594923f00f626f4f2
SamueleLettieri <samuele2073@gmail.com>
2022-07-17 17:16:54 +0200

    sistemato errore nel bonus

--- src/components/DCMainExtension.vue
+++ src/components/DCMainExtension.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="ms_background-blue p-5">
-    <div class="container p-1">
-        <div class="d-flex">
-            <div class="mx-4"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-comics-digital-comics.png" alt="">DIGITAL COMICS</a></div>
-            <div class="mx-4"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-comics-merchandise.png" alt="">DC MERCHANDISE</a></div>
-            <div class="mx-4"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-comics-shop-locator.png" alt="">SUBSCRIPTION</a></div>
-            <div class="mx-4"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-comics-subscriptions.png" alt="">COMIC SHOP LOCATOR</a></div>
-            <div class="ms-4"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-dc-power-visa.svg" alt="">DC POWER VISA</a></div>
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <div class="mx-4 ms_font"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-comics-digital-comics.png" alt="">DIGITAL COMICS</a></div>
+            <div class="mx-4 ms_font"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-comics-merchandise.png" alt="">DC MERCHANDISE</a></div>
+            <div class="mx-4 ms_font"><a class="ms_link" href="#"><img class="me-1 ms_img" src="../assets/img/buy-comics-subscriptions.png" alt="">SUBSCRIPTION</a></div>
+            <div class="mx-4 ms_font"><a class="ms_link" href="#"><img class="me-1 ms_img-locator" src="../assets/img/buy-comics-shop-locator.png" alt="">COMIC SHOP LOCATOR</a></div>
+            <div class="ms-4 ms_font"><a class="ms_link" href="#"><img class="me-1 ms_img-locator" src="../assets/img/buy-dc-power-visa.svg" alt="">DC POWER VISA</a></div>
         </div>
     </div>
   </div>
@@ -29,8 +29,16 @@ export default {
     background-color: $colorplus;
 }
 
+.ms_font{
+    font-size: 12px;
+}
+
 .ms_img{
     width: 25%
+}
+
+.ms_img-locator{
+    width: 17%;
 }
 
 .ms_link{
